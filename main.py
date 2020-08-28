d52c2778284223c509912bede71b29dfd95d70e7
Matthias Keim <info@matthias-keim.com>
2020-08-28 09:32:18 +0200

    added arrow keys

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import serial
 import keyboard
+import platform
+
+
+if platform.system() == "Darwin":
+    LEFT_KEY = 123
+    UP_KEY = 126
+    RIGHT_KEY = 124
+    DOWN_KEY = 125
+
+if platform.system() == "Windows":
+    LEFT_KEY = 75
+    UP_KEY = 72
+    RIGHT_KEY = 72
+    DOWN_KEY = 80
 
 ser = serial.Serial('/dev/tty.usbmodemS_N__G16M02291', timeout=1)
 
