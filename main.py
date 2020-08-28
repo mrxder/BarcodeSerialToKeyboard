01fe89a869791bb234ed39667f15ba52a6ad7e0f
Matthias Keim <info@matthias-keim.com>
2020-08-28 12:03:08 +0200

    added gui

--- main.py
+++ main.py
@@ -2,41 +2,62 @@ import serial
 import keyboard
 import platform
 import time
+import tkinter as tk
+from threading import Thread
 
 
-if platform.system() == "Darwin":
-    LEFT_KEY = 123
-    UP_KEY = 126
-    RIGHT_KEY = 124
-    DOWN_KEY = 125
-
-if platform.system() == "Windows":
-    LEFT_KEY = "left arrow"
-    UP_KEY = "up arrow"
-    RIGHT_KEY = "right arrow"
-    DOWN_KEY = "down arrow"
-
-#ser = serial.Serial('/dev/tty.usbmodemS_N__G16M02291', timeout=1)
-ser = serial.Serial('COM3', timeout=0.1)
-
-
-try:
+def test(x):
     while True:
-        line = ser.readline()
-        if len(line) > 0:
-            line_str = line.decode('utf-8')  # last char is /n
-            print(line_str)
-            keyboard.write(line_str)
-            keyboard.send("right arrow")
-            keyboard.send("down arrow")
-            time.sleep(0.5)
-            keyboard.send("left arrow")
-            keyboard.send("left arrow")
-            keyboard.send("left arrow")
-except:
+        val = x.get()
+        if len(val) > 0:
+            print(x.get())
+        time.sleep(1)
+
+
+window = tk.Tk(className="Scanner Control")
+window.geometry("500x200")
+# window.configure(bg='blue')
+greeting = tk.Label(text="Kostenkonto nr", bg="yellow")
+entry = tk.Entry()
+
+greeting.pack()
+entry.pack()
+
+scannerThread = Thread(target=test, args=(entry,))
+scannerThread.start()
+
+window.mainloop()
+exit()
+
+
+deg Scanner(kostenKontoEntry):
+    ser = serial.Serial('COM3', timeout=0.1)
+
+    try:
+        while True:
+            line = ser.readline()
+            if len(line) > 0:
+                line_str = line.decode('utf-8')  # last char is /n
+                print(line_str)
+
+                kostenKonto = kostenKontoEntry.get()
+
+                if len(kostenKonto) > 0:
+                    # blub
+                else:
+                    keyboard.write(line_str)
+                    keyboard.send("right arrow")
+                    keyboard.send("down arrow")
+                    time.sleep(0.5)
+                    keyboard.send("left arrow")
+                    keyboard.send("left arrow")
+                    keyboard.send("left arrow")
+    except:
+
+        ser.close()
+        print("Closed by Exception")
+        exit()
 
     ser.close()
-    print("Closed by Exception")
-
-ser.close()
-print("Closed by end of program")
+    print("Closed by end of program")
+    exit()
